19c462fd959f9a263d3f508f6757c1cec545856a
Madared <77392785+Madared@users.noreply.github.com>
2023-06-23 23:37:53 +0100

    Create HelpfulExtensions.cs

---
+++ HelpfulExtensions.cs
@@ -0,0 +1,43 @@
+namespace Results;
+
+public static class HelpfulExtensions
+{
+    public static Result<TOut> Pipe<TIn, TOut>(this TIn? i, Func<TIn, Result<TOut>> function) where TIn : class where TOut : class
+    {
+        if (i is null)
+            return Result<TOut>.Fail(new UnknownError());
+        return function(i);
+    }
+
+    public static TOut? Pipe<TIn, TOut>(this TIn? i, Func<TIn, TOut?> function)
+        where TIn : class
+        where TOut : class
+    {
+        if (i is null)
+            return null;
+        return function(i);
+    }
+
+    public static Result Pipe<TIn>(this TIn? i, Func<TIn, Result> function, IError error)
+        where TIn : class
+    {
+        if (i is null)
+            return Result.Fail(error);
+        return function(i);
+    }
+
+    public static Result Pipe(this Result i, Func<Result> action)
+    {
+        if (i.Failed)
+            return i;
+        return action();
+    }
+
+    public static Result<TIn> ToResult<TIn>(this TIn? i, IError error)
+        where TIn : class =>
+        Result<TIn>.Unknown(i, error);
+
+    public static Result<TIn> ToResult<TIn>(this TIn i)
+        where TIn : class =>
+        Result<TIn>.Ok(i);
+}
