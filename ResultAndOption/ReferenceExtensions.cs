65e35285139fe111f27135b29cc0abff2645f28f
Dex <77392785+Madared@users.noreply.github.com>
2023-07-31 12:50:47 +0100

    Fixed ListMap naming

--- ResultAndOption/ReferenceExtensions.cs
+++ ResultAndOption/ReferenceExtensions.cs
@@ -106,7 +106,7 @@ public static class ReferenceExtensions
     /// <param name="list">The input list.</param>
     /// <param name="function">The mapping function to apply to each element in the list.</param>
     /// <returns>A list of the mapped values.</returns>
-    public static List<TOut> Map<TIn, TOut>(this List<TIn> list, Func<TIn, TOut> function) =>
+    public static List<TOut> ListMap<TIn, TOut>(this List<TIn> list, Func<TIn, TOut> function) =>
         list
             .Select(function)
             .ToList();
