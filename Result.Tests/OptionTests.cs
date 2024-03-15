f33b18c96f316f98a22cad09a24ea912330f12df
Dex <77392785+Madared@users.noreply.github.com>
2024-03-15 10:24:11 +0000

    Added basic tests to check option MapAsync functionality

--- Result.Tests/OptionTests.cs
+++ Result.Tests/OptionTests.cs
@@ -190,4 +190,54 @@ public class OptionTests {
         Option<string> optionalString = existingString.ToOption();
         Assert.True(optionalString.IsSome());
     }
+
+    [Fact]
+    public void Async_Option_Map_Returns_Correct_Value() {
+        string hello = "hello";
+        Task<Option<string>> taskOption = Task.FromResult(Option<string>.Some(hello));
+        Task<Option<int>> intTaskOption = taskOption.MapAsync(s => s.Length);
+        Option<int> intOption = intTaskOption.Result;
+        Assert.True(intOption.IsSome());
+        Assert.Equal(hello.Length, intOption.Data);
+    }
+
+    [Fact]
+    public void Async_Option_Map_With_Async_Mapper_Returns_Correct_Value() {
+        string hello = "hello";
+        Task<Option<string>> taskOption = Task.FromResult(Option<string>.Some(hello));
+
+        async Task<int> SomeAsyncFunction(string s) {
+            return s.Length;
+        }
+
+        Task<Option<int>> intTaskOption = taskOption.MapAsync(SomeAsyncFunction);
+        Option<int> intOption = intTaskOption.Result;
+        Assert.True(intOption.IsSome());
+        Assert.Equal(hello.Length, intOption.Data);
+    }
+
+    [Fact]
+    public void Async_Option_Map_Unwraps_Nested_Option_Result() {
+        string hello = "hello";
+        Task<Option<string>> taskOption = Task.FromResult(Option<string>.Some(hello));
+        Task<Option<int>> intTaskOption = taskOption.MapAsync(s => Option<int>.Some(s.Length));
+        Option<int> intOption = intTaskOption.Result;
+        Assert.True(intOption.IsSome());
+        Assert.Equal(hello.Length, intOption.Data);
+    }
+
+    [Fact]
+    public void Async_Option_Map_Unwraps_Nested_Async_Option_Result() {
+        string hello = "hello";
+        Task<Option<string>> taskOption = Task.FromResult(Option<string>.Some(hello));
+
+        async Task<Option<int>> SomeAsyncFunction(string s) {
+            return Option<int>.Some(s.Length);
+        }
+
+        Task<Option<int>> intTaskOption = taskOption.MapAsync(SomeAsyncFunction);
+        Option<int> intOption = intTaskOption.Result;
+        Assert.True(intOption.IsSome());
+        Assert.Equal(hello.Length, intOption.Data);
+    }
 }
