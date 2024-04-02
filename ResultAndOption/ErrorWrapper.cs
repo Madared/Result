b97e5f1b7d931cd308973ee00f6ac8f924ae1923
Dex <77392785+Madared@users.noreply.github.com>
2024-04-02 19:41:51 +0100

    Created ErrorWrapper

--- ResultAndOption/ErrorWrapper.cs
+++ ResultAndOption/ErrorWrapper.cs
@@ -1,5 +1,13 @@
 namespace Results;
 
-public class ErrorWrapper {
-    
+public class ErrorWrapper : Exception {
+    public ErrorWrapper(IError error) : base(error.Message) {
+    }
+
+    public static Exception Create(IError error) {
+        if (error is ExceptionWrapper ex) {
+            return ex.Exception;
+        }
+        else return new ErrorWrapper(error);
+    }
 }
