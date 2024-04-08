ceea946f56375eaa4ef2df9144d4d48747a660b2
Dex <77392785+Madared@users.noreply.github.com>
2024-04-08 20:20:53 +0100

    Added basic tests for simple successful starting context

--- Result.Tests/ContextFromSuccessSimpleResult.cs
+++ Result.Tests/ContextFromSuccessSimpleResult.cs
@@ -1,9 +1,53 @@
 namespace ResultTests;
 
-public class SuccessSimpleStartingContext {
+public class ContextFromSuccessSimpleResult {
     private static Func<Result> resultFunc = Result.Ok;
     private static IContextResult context = resultFunc.RunAndGetContext();
-    
+
     [Fact]
-    public void Context_Is_Success
+    public void Context_Is_Success() {
+        Assert.True(context.Succeeded);
+    }
+
+    [Fact]
+    public void Mapping_With_Successful_Simple_Result_Function_Gives_Successful_Context() {
+        IContextResult mapped = context.Map(Result.Ok);
+        Assert.True(mapped.Succeeded);
+    }
+
+    [Fact]
+    public void Mapping_With_Successful_Complex_Result_Function_Gives_Successful_Context() {
+        IContextResult<string> mapped = context.Map(() => Result<string>.Ok("success"));
+        Assert.True(mapped.Succeeded);
+    }
+
+    [Fact]
+    public void Mapping_With_Action_Gives_Successful_Context() {
+        IContextResult mapped = context.Map(() => Console.WriteLine("hello"));
+        Assert.True(mapped.Succeeded);
+    }
+
+    [Fact]
+    public void Mapping_With_Not_Null_Function_Gives_Successful_Context() {
+        IContextResult<string> mapped = context.Map(() => "hello");
+        Assert.True(mapped.Succeeded);
+    }
+
+    [Fact]
+    public void Mapping_With_Failed_Simple_Result_Function_Gives_Failed_Context() {
+        IContextResult mapped = context.Map(() => Result.Fail(new UnknownError()));
+        Assert.True(mapped.Failed);
+    }
+
+    [Fact]
+    public void Mapping_With_Failed_Complex_Result_Function_Gives_Failed_Context() {
+        IContextResult<string> mapped = context.Map(() => Result<string>.Fail(new UnknownError()));
+        Assert.True(mapped.Failed);
+    }
+
+    [Fact]
+    public void Stripping_Context_Gives_Same_Result() {
+        Result stripped = context.StripContext();
+        Assert.Equal(resultFunc(), stripped);
+    }
 }
