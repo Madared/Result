95e2ff5c74de5ee243ee43bf1426ffeec3fe514b
Dex <77392785+Madared@users.noreply.github.com>
2024-04-09 19:24:06 +0100

    Added public IError property in ErrorWrapper

--- ResultAndOption/ErrorWrapper.cs
+++ ResultAndOption/ErrorWrapper.cs
@@ -1,9 +1,10 @@
 namespace Results;
 
 public class ErrorWrapper : Exception {
+    public IError Error { get; }
     public ErrorWrapper(IError error) : base(error.Message) {
+        Error = error;
     }
-
     public static Exception Create(IError error) {
         if (error is ExceptionWrapper ex) {
             return ex.Exception;
