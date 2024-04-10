0313036504f25eb82be3bef4df2902fdc13e771c
Dex <77392785+Madared@users.noreply.github.com>
2024-04-10 20:48:37 +0100

    Added RunAndGetContext overloads with more parameters

--- ResultAndOption/ContextResults/FuncExtensions.cs
+++ ResultAndOption/ContextResults/FuncExtensions.cs
@@ -26,6 +26,40 @@ public static class FuncExtensions {
 
     public static IContextResult<TOut> RunAndGetContext<TOut>(this Func<Result<TOut>> function) where TOut : notnull => new StartingContextResult<TOut>(function(), function);
 
+    public static IContextResult<TOut> RunAndGetContext<T1, TOut>(this Func<T1, Result<TOut>> function, T1 input1)
+        where T1 : notnull
+        where TOut : notnull {
+        Func<Result<TOut>> generated = () => function(input1);
+        return new StartingContextResult<TOut>(generated(), generated);
+    }
+
+    public static IContextResult<TOut> RunAndGetContext<T1, T2, TOut>(this Func<T1, T2, Result<TOut>> function, T1 input1, T2 input2)
+        where T1 : notnull
+        where T2 : notnull
+        where TOut : notnull {
+        return new StartingContextResult<TOut>(Generated(), Generated);
+        Result<TOut> Generated() => function(input1, input2);
+    }
+
+    public static IContextResult<TOut> RunAndGetContext<T1, T2, T3, TOut>(this Func<T1, T2, T3, Result<TOut>> function, T1 input1, T2 input2, T3 input3)
+        where T1 : notnull
+        where T2 : notnull
+        where T3 : notnull
+        where TOut : notnull {
+        return new StartingContextResult<TOut>(Generated(), Generated);
+        Result<TOut> Generated() => function(input1, input2, input3);
+    }
+
+    public static IContextResult<TOut> RunAndGetContext<T1, T2, T3, T4, TOut>(this Func<T1, T2, T3, T4, Result<TOut>> function, T1 input1, T2 input2, T3 input3, T4 input4)
+        where T1 : notnull
+        where T2 : notnull
+        where T3 : notnull
+        where T4 : notnull
+        where TOut : notnull {
+        return new StartingContextResult<TOut>(Generated(), Generated);
+        Result<TOut> Generated() => function(input1, input2, input3, input4);
+    }
+    
     public static IContextResult Retry(this IContextResult context,  int timesToRetry) {
         int timesRetried = 0;
         while (timesRetried < timesToRetry) {
