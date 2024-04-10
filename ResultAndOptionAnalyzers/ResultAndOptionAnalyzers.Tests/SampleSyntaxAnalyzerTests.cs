c782d71a7fc1f6d0ad1090a9ceae11672cfdda34
Dex <77392785+Madared@users.noreply.github.com>
2024-04-10 01:44:19 +0100

    Added analyzer project

---
+++ ResultAndOptionAnalyzers/ResultAndOptionAnalyzers.Tests/SampleSyntaxAnalyzerTests.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Xunit;
+using Verifier =
+    Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+        ResultAndOptionAnalyzers.SampleSyntaxAnalyzer>;
+
+namespace ResultAndOptionAnalyzers.Tests;
+
+public class SampleSyntaxAnalyzerTests {
+    [Fact]
+    public async Task ClassWithMyCompanyTitle_AlertDiagnostic() {
+        const string text = @"
+public class MyCompanyClass
+{
+}
+";
+
+        var expected = Verifier.Diagnostic()
+            .WithLocation(2, 14)
+            .WithArguments("MyCompanyClass");
+        await Verifier.VerifyAnalyzerAsync(text, expected).ConfigureAwait(false);
+    }
+}
