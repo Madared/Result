b245f6046950a62e24527777048bf1349f5f39c5
Dex <77392785+Madared@users.noreply.github.com>
2024-04-22 18:02:26 +0100

    Removed unused global using

--- Context.Tests/GlobalUsings.cs
+++ Context.Tests/GlobalUsings.cs
@@ -1,8 +1,4 @@
 global using Xunit;
-global using Context;
-global using Context.ContextCallables;
 global using Context.ContextResults;
 global using Context.ContextCommands;
-global using Context.ActionCallables;
-global using Context.CallableGenerators;
 global using Context.ContextResults.ContextResultExtensions;
