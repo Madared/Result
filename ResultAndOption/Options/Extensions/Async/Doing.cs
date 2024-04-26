ceb222888e716b65c1be0510286df6499df5c1ab
Afonso <afonsomtsm@gmail.com>
2024-04-26 17:02:40 +0100

    Added DoAsync option methods with cancellation tokens

--- ResultAndOption/Options/Extensions/Async/Doing.cs
+++ ResultAndOption/Options/Extensions/Async/Doing.cs
@@ -1,5 +1,7 @@
 namespace ResultAndOption.Options.Extensions.Async;
 
+public delegate Task<Option<T>> AsyncActionWithInput<T>(T data, CancellationToken? token = null);
+
 public static class Doing
 {
     /// <summary>
@@ -13,5 +15,67 @@ public static class Doing
     {
         Option<T> originalOption = await option;
         return originalOption.Do(action);
+    }
+
+    public static async Task<Option<T>> DoAsync<T>(this Task<Option<T>> option, Action action) where T : notnull
+    {
+        Option<T> originalOption = await option;
+        return originalOption.Do(action);
+    }
+
+    public static async Task<Option<T>> DoAsync<T>(
+        this Option<T> option,
+        AsyncActionWithInput<T> actionWithInput,
+        CancellationToken? token = null) where T : notnull
+    {
+        if (option.IsSome())
+        {
+            await actionWithInput(option.Data, token);
+        }
+
+        return option;
+    }
+
+    public static async Task<Option<T>> DoAsync<T>(
+        this Task<Option<T>> option,
+        AsyncActionWithInput<T> actionWithInput,
+        CancellationToken? token = null) where T : notnull
+    {
+        Option<T> originalOption = await option;
+        if (originalOption.IsSome())
+        {
+            await actionWithInput(originalOption.Data, token);
+        }
+
+        return originalOption;
+    }
+
+    public delegate Task<Option<T>> AsyncAction<T>(CancellationToken? token = null);
+
+    public static async Task<Option<T>> DoAsync<T>(
+        this Option<T> option,
+        AsyncAction<T> action,
+        CancellationToken? token = null) where T : notnull
+    {
+        if (option.IsSome())
+        {
+            await action(token);
+        }
+
+        return option;
+    }
+
+    public static async Task<Option<T>> DoAsync<T>(
+        this Task<Option<T>> option,
+        AsyncAction<T> action,
+        CancellationToken? token = null) where T : notnull
+    {
+        Option<T> originalOption = await option;
+        if (originalOption.IsSome())
+        {
+            await action(token);
+        }
+
+        return originalOption;
     }
 }
