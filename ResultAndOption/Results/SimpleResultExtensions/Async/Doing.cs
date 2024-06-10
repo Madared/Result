38daf041e51844e52e8e30371b834e32d666eaea
Afonso <afonsomtsm@gmail.com>
2024-06-10 04:22:56 +0100

    Added CancellationToken

--- ResultAndOption/Results/SimpleResultExtensions/Async/Doing.cs
+++ ResultAndOption/Results/SimpleResultExtensions/Async/Doing.cs
@@ -7,25 +7,26 @@ namespace ResultAndOption.Results.SimpleResultExtensions.Async;
 /// </summary>
 public static class Doing
 {
-     /// <summary>
+    /// <summary>
     /// Awaits the result, if its a failure, awaits the action with its error as a param and returns the result,
     /// otherwise just returns the result 
     /// </summary>
     /// <param name="result"></param>
     /// <param name="action"></param>
     /// <returns>The same result</returns>
-    
-
     /// <summary>
     /// If the result is a failure awaits the specified action and returns its result, otherwise returns the same result
     /// </summary>
     /// <param name="result"></param>
     /// <param name="mapper"></param>
     /// <returns></returns>
-    public static async Task<Result> DoAsync(this Result result, Func<Task<Result>> mapper) => result.Failed
+    public static async Task<Result> DoAsync(
+        this Result result,
+        Func<CancellationToken?, Task<Result>> mapper,
+        CancellationToken? cancellationToken = null) => result.Failed
         ? result
-        : await mapper();
-    
+        : await mapper(cancellationToken);
+
     /// <summary>
     /// Awaits the result and if its a success awaits the asyncMapper otherwise returns the same result
     /// </summary>
@@ -35,7 +36,7 @@ public static class Doing
     public static async Task<Result> DoAsync(
         this Task<Result> result,
         Func<CancellationToken?, Task<Result>> asyncMapper,
-        CancellationToken? token)
+        CancellationToken? token = null)
     {
         Result originalResult = await result;
         return originalResult.Failed ? originalResult : await asyncMapper(token);
