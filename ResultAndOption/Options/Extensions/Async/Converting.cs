14df7536936fe0f436514bba4a99adc450f44126
Afonso <afonsomtsm@gmail.com>
2024-12-21 11:16:10 +0000

    Added OrAsync

--- ResultAndOption/Options/Extensions/Async/Converting.cs
+++ ResultAndOption/Options/Extensions/Async/Converting.cs
@@ -16,4 +16,42 @@ public static class Converting
         T? data = await nullable;
         return data.ToOption();
     }
+
+    /// <summary>
+    /// Provides a fallback for an async option in case it is empty.
+    /// </summary>
+    /// <param name="asyncOption"></param>
+    /// <param name="fallback"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static async Task<T> OrAsync<T>(this Task<Option<T>> asyncOption, T fallback) where T : notnull
+    {
+        Option<T> option = await asyncOption;
+        return option.IsSome() ? option.Data : fallback;
+    }
+
+    /// <summary>
+    /// Returns the existing option if it is not empty otherwise turns the replacement value into an option and returns it
+    /// </summary>
+    /// <param name="asyncOption"></param>
+    /// <param name="replacement"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static async Task<Option<T>> OrNullableAsync<T>(this Task<Option<T>> asyncOption, T? replacement) where T : notnull
+    {
+        Option<T> option = await asyncOption;
+        return option.IsNone() ? Option<T>.Maybe(replacement) : option;
+    }
+
+    /// <summary>
+    /// Returns this option if it is not empty, otherwise returns the passed in option
+    /// </summary>
+    /// <param name="asyncOption"></param>
+    /// <param name="replacement">replacement option</param>
+    /// <returns></returns>
+    public static async Task<Option<T>> OrOption<T>(this Task<Option<T>> asyncOption, Option<T> replacement) where T : notnull
+    {
+        Option<T> option = await asyncOption;
+        return option.IsNone() ? replacement : option;
+    }
 }
